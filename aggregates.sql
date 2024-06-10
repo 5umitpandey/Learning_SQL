1694605fd96b7279057e8b19cf2d0bc5ab8266c8
Sumit Pandey <68957789+sumi1pandey@users.noreply.github.com>
2024-06-10 07:56:31 +0530

    Update aggregates.sql

--- aggregates.sql
+++ aggregates.sql
@@ -1,3 +1,7 @@
 --Find the total number of employee years worked in each building
 SELECT Building, SUM(Years_employed) FROM employees
 GROUP BY Building
+
+--Find the number of Employees of each role in the studio 
+SELECT Count (Role), Role FROM employees
+GROUP BY Role
