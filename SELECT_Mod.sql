ced84d27e3983cb490763f69d05725f221bdfdbf
Sumit Pandey <68957789+sumi1pandey@users.noreply.github.com>
2024-06-14 18:50:44 +0530

    Update SELECT_Mod.sql

--- SELECT_Mod.sql
+++ SELECT_Mod.sql
@@ -19,3 +19,10 @@ ORDER BY longitude ASC;
 SELECT Director, Count(Title) as NumberOfMovies
 FROM movies
 Group BY Director
+
+--Find the total domestic and international sales that can be attributed to each director
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+FROM movies
+    INNER JOIN boxoffice
+        ON movies.id = boxoffice.movie_id
+GROUP BY director;
