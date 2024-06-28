a7e5501db5d2d2b28d33daa20d965e09ff30f5e4
Sumit Pandey <68957789+sumi1pandey@users.noreply.github.com>
2024-06-28 14:28:03 +0530

    Create set_operations.sql

---
+++ set_operations.sql
@@ -0,0 +1,9 @@
+--Add Real Example Later!
+
+SELECT column, another_column
+   FROM mytable
+UNION / UNION ALL / INTERSECT / EXCEPT
+SELECT other_column, yet_another_column
+   FROM another_table
+ORDER BY column DESC
+LIMIT n;
