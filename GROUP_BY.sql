ef53b9f6027fb8314990634e5823e1a7fb4633ff
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2024-08-05 08:22:28 +0530

    Update GROUP_BY.sql

--- GROUP_BY.sql
+++ GROUP_BY.sql
@@ -1,7 +1,14 @@
+--https://learnsql.com/course/sql-queries/order-by-group-by/group-by/group-by-multiple-columns/
+
 --Find the average salary for each employee. Show the last name, the first name, and the average salary.
 --Group the table by the last name and the first name.
-
---https://learnsql.com/course/sql-queries/order-by-group-by/group-by/group-by-multiple-columns/
 SELECT AVG(salary), first_name, last_name
 FROM employees
 GROUP BY first_name, last_name
+
+--https://learnsql.com/course/sql-queries/order-by-group-by/group-by/group-by-multiple-columns/
+--Find the average salary for each employee. Show the last name, the first name, and the average salary.
+--Group the table by the last name and the first name.
+SELECT AVG(salary), last_name, first_name
+FROM employees
+GROUP BY last_name, first_name
