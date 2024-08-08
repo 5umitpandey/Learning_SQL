71adde0c1f3b0b5be6d9e0a118b29adf4e7c94c6
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2024-08-08 22:48:26 +0530

    Update HAVING.sql

--- HAVING.sql
+++ HAVING.sql
@@ -6,3 +6,11 @@ SELECT last_name, first_name, COUNT(DISTINCT year) as years
 FROM employees
 GROUP BY Last_name, First_name
 HAVING COUNT(year) > 2
+
+--https://learnsql.com/course/sql-queries/order-by-group-by/having/having-continued/
+--Find such departments where the average salary in 2012 was higher than $3,000. Show the department name with the average salary.
+SELECT DEPARTMENT, AVG(SALARY)
+FROM EMPLOYEES
+WHERE YEAR = 2012
+GROUP BY DEPARTMENT
+HAVING AVG(SALARY) > 3000
