41944234b386d8e17dd038d4478536c00eecede9
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2024-08-10 17:30:45 +0530

    Update HAVING.sql

--- HAVING.sql
+++ HAVING.sql
@@ -21,3 +21,12 @@ SELECT last_name, first_name, SUM(salary)
 FROM employees
 GROUP BY last_name, first_name
 ORDER BY SUM(salary) DESC
+
+--https://learnsql.com/course/sql-queries/order-by-group-by/practise/practise/
+--Show the columns last_name and first_name from the table employees together with each person's average salary and the number of years they (have) worked in the company.
+--Use the following aliases: average_salary for each person's average salary and years_worked for the number of years worked in the company. Show only such employees who (have) spent more than 2 years in the company. Order the results according to the average salary in the descending order.
+SELECT last_name, first_name, AVG(salary) as average_salary, COUNT(DISTINCT(year)) as years_worked
+FROM employees
+GROUP BY last_name, first_name
+HAVING COUNT(year) > 2
+ORDER BY AVG(salary) DESC
