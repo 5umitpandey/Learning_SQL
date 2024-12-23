98ae364621fbb06f5f11e9f5dc2c7673c1bc36df
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2024-12-23 13:01:27 +0530

    Update Subqueries_in_FROM_Clause.sql

--- Subqueries_in_FROM_Clause.sql
+++ Subqueries_in_FROM_Clause.sql
@@ -9,3 +9,18 @@ FROM Mountain, (
     WHERE Population >= 50000
 ) AS small_country
 WHERE small_country.id = Mountain.country_id;
+
+
+
+--https://learnsql.com/course/sql-queries/subqueries/other-subqueries/subquery-from-2/
+--Show hiking trips together with their mountains. The mountains must be at least 3,000 high. Select only the columns length and height
+--Query inside FROM
+SELECT
+  Length,
+  Height
+FROM Hiking_trip, (
+  SELECT *
+  FROM Mountain
+  WHERE Height >= 3000
+) AS nice_Mountain
+WHERE nice_Mountain.id = Hiking_trip.Mountain_id;
